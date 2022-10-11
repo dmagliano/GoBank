cf0f0bae5465a3914e4856bccb7206a920a134a5
Diogo Magliano <diogo.magliano@gmail.com>
2022-10-11 20:03:35 -0300

    define main func and set up server address

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
-package GoBank
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		println("request to hello")
+		fmt.Fprintln(writer, "Hello World")
+	})
+
+	http.ListenAndServe("localhost:8000", nil)
 
 }
