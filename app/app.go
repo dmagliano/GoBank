fbbd52c478da66d8be37a51c054e2fe81350402a
Diogo Magliano <diogo.magliano@gmail.com>
2022-10-11 23:52:42 -0300

    Pattern matchers and methods. Go.sum addedg

--- app/app.go
+++ app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -11,10 +12,21 @@ func Start() {
 	router := mux.NewRouter()
 
 	//defining routes
-	router.HandleFunc("/hello", hello)
-	router.HandleFunc("/customers", getAllCustomer)
+	router.HandleFunc("/hello", hello).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomer).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	//starting server
 	log.Fatalf("error", http.ListenAndServe("localhost:8000", router))
 
+}
+
+func createCustomer(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer, "Post request received")
+}
+
+func getCustomer(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	fmt.Fprint(writer, vars["customer_id"])
 }
